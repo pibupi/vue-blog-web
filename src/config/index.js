5f55c37e50741970f46493646c4767da54911c86
zhanghe <15942892352@sina.cn>
2019-12-04 11:16:04 +0800

    update:修改生产ip

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,4 @@
 export const baseURL =
-  process.env.NODE_ENV === "production" ? "/api/" : "http://localhost:5001";
+  process.env.NODE_ENV === "production"
+    ? "http://39.105.218.164:5001"
+    : "http://localhost:5001";
