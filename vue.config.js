5ffe3c9325c16ed6b18198da6dad4e9479cc5ea5
zhanghe <15942892352@sina.cn>
2020-03-09 17:44:03 +0800

    update:readme

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,29 @@
+// module.exports = {
+//   // 以下是pwa配置
+//   pwa: {
+//     iconPaths: {
+//       favicon32: 'github.ico',
+//       favicon16: 'github.ico',
+//       appleTouchIcon: 'github.ico',
+//       maskIcon: 'github.ico',
+//       msTileImage: 'github.ico'
+//     }
+//   },
+//   configureWebpack: config => {
+//     config.externals = {
+//       vue: 'Vue',
+//       'vue-router': 'VueRouter',
+//       "highlight.js": "hljs",
+//       lodash: {
+//         commonjs: 'lodash',
+//         umd: 'lodash',
+//         root: '_'
+//       }
+//     };
+//   }
+// };
+// const BundleAnalyzer = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 module.exports = {
   // 以下是pwa配置
   pwa: {
@@ -9,15 +35,54 @@ module.exports = {
       msTileImage: 'github.ico'
     }
   },
-  configureWebpack: config => {
-    config.externals = {
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      lodash: {
-        commonjs: 'lodash',
-        umd: 'lodash',
-        root: '_'
+  configureWebpack: {
+    externals: {
+      'highlight.js': 'hljs'
+    }
+    // plugins: [new BundleAnalyzer()]
+  },
+  chainWebpack: config => {
+    config.optimization.splitChunks({
+      cacheGroups: {
+        vendors: {
+          name: 'chunk-vendors',
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          chunks: 'initial'
+        },
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        },
+        elementUi: {
+          name: 'elementUi',
+          test: /[\\/]node_modules[\\/](element-ui)[\\/]/,
+          chunks: 'all'
+        },
+        moment: {
+          name: 'moment',
+          test: /[\\/]node_modules[\\/](moment)[\\/]/,
+          chunks: 'all'
+        },
+        coreJs: {
+          name: 'coreJs',
+          test: /[\\/]node_modules[\\/](core-js)[\\/]/,
+          chunks: 'all'
+        },
+        marked: {
+          name: 'marked',
+          test: /[\\/]node_modules[\\/](marked)[\\/]/,
+          chunks: 'all'
+        },
+        axios: {
+          name: 'axios',
+          test: /[\\/]node_modules[\\/](axios)[\\/]/,
+          chunks: 'all'
+        }
       }
-    };
+    });
   }
 };
